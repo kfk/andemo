c960223377a3e7488c2b60a588edb0c346023300
kfk <alessiocivitillo@gmail.com>
2012-04-05 22:58:31 +0200

    c

--- optiver_ml/lib/yahoo_stocks.py
+++ optiver_ml/lib/yahoo_stocks.py
@@ -1,61 +1,53 @@
 import urllib2 
 import calendar, time
 
-def curr_quote(stock_id):
-	header = ['name','id','open','close']
-	url = 'http://download.finance.yahoo.com/d/quotes.csv?s='+stock_id+'&f=nsop&e=.csv'
-	data = urllib2.urlopen(url)
-	d = {}
-	l = []
-	for item in data:
-		n = -1
-		td = {}
-		item = item.replace('"','')
-		item = item.split(',')
-		for k in header:
-			n+=1
-			td[k] = item[n]
-		l.append(td)
-	d['sorted_keys'] = header
-	d['data'] = l
-	return d
-import datetime
+def tran_f(item):
+	print item
+	item = item.split('-')
+	item = calendar.timegm((int(item[0]),int(item[1]),int(item[2]),0,0,0,0,0))*1000
+	return item
 
-def hist_quote(stock_id):
-	header = ['date','open', 'close']
-	d = {'stock_id':stock_id, 'from_m':'1', 'from_d':'1', 'from_y':'2007', 'to_m':'2', 'to_d':'28', 'to_y':'2012', 'interval':'w'}
-	url = 'http://ichart.yahoo.com/table.csv?s={stock_id}&a={from_m}&b={from_d}&c={from_y}&d={to_m}&e={to_d}&f={to_y}&g={interval}&ignore=.csv'.format(**d)
+def yh_stock_api_get(d):
+	'Gets a hash table with url and url params: {url_1:{param1:xyz,parm2:zvx}}'
+	rd = {}
+	try:
+		rd['header'] = d['header']
+	except KeyError:
+		rd['header']=False
+		
+	url = d['url'].format(**d['url_params'])
 	data = urllib2.urlopen(url)
 	l = []
-	l_last = []
-	d_last = {}
 	n = -1
 	for row in data:
 		n+=1
-		if n >= 1:
-			tl = []
-			row = row.split(',')
-			time = row[0].split('-')
-			time = calendar.timegm((int(time[0]),int(time[1]),int(time[2]),0,0,0,0,0))*1000
-			tl.append(time) 
-			tl.append(float(row[-1]))
-			l.append(tl)
-			
-			n2 = -1
-			td = {}
-			for k in header:
-				n2+=1
-				td[k] = row[n2]	
-			td['&Delta;'] = '%.2f' % (float(td['open'])-float(td['close'])) 
-			td['&Delta;%'] = '%.2f' % (float(td['&Delta;'])/float(td['open'])*100)
-			l_last.append(td)
-	for item in ['&Delta;','&Delta;%']:
-		header.append(item)
-	d_last['data'] = l_last
-	d_last['sorted_keys'] = header
-	l.reverse()
-	l.pop(-1)
-	return l, d_last
+		row = row.split(',')
+		if rd['header'] != False:	
+			row[0] = tran_f(row[0])
+			l.append(row)
+		else:
+			if n>0:
+				row[0] = tran_f(row[0])
+				l.append(row)
+			else:
+				rd['header'] = row
+	rd['values'] = l
+	return rd
 
-hist_quote('GOOG')
+def d_to_html_table(d, table_id=''):
+	out_h = ''
+	for item in d['header']:
+		out_h += '''<th>{}</th>\n'''.format(item)
+
+	out_b = ''	
+	for row in d['values']:
+		out_b+='<tr>\n'
+		for item in row:	
+			out_b+='<td>{}</td>\n'.format(item)
+		out_b+='</tr>\n'
+	html_table = '''<table class='table table-bordered table-striped' id='''+table_id+'''>''' + '<thead>' + out_h +'</thead>' + '<tbody>' + out_b + '</tbody>' + '</table>\n'
+	return html_table
+
+#ld = {'name':'values','url':'http://download.finance.yahoo.com/d/quotes.csv?s={stock_id}&f=nsop&e=.csv','url_params':{'stock_id':'GOOG'},'header':['name','id','open','close']}
+ld = {'url':'http://ichart.yahoo.com/table.csv?s={stock_id}&a={from_m}&b={from_d}&c={from_y}&d={to_m}&e={to_d}&f={to_y}&g={interval}&ignore=.csv','url_params':{'stock_id':'GOOG', 'from_m':'1', 'from_d':'1', 'from_y':'2007', 'to_m':'2', 'to_d':'28', 'to_y':'2012', 'interval':'w'}}
 
--- optiver_ml/views/views.py
+++ optiver_ml/views/views.py
@@ -24,16 +24,23 @@ def index():
  
 	return render_template('index.html', form=form, curr_quote=curr_quote,l_hist=l_hist, d_last_hist=d_last_hist)
 
-def d_to_html(d, table_id=''):	
-	out_h = ''
-	for k in d['sorted_keys']:
-		out_h += '''<th>{}</th>\n'''.format(k)
-	
-	out_b = ''	
-	for row in d['data']:
-		out_b+='<tr>\n'
-		for k in d['sorted_keys']:	
-			out_b+='<td>{}</td>\n'.format(row[k])
-		out_b+='</tr>\n'
-	html_table = '''<table class='table table-bordered table-striped' id='''+table_id+'''>''' + '<thead>' + out_h +'</thead>' + '<tbody>' + out_b + '</tbody>' + '</table>\n'
-	return html_table
+ld = {'url':'http://ichart.yahoo.com/table.csv?s={stock_id}&a={from_m}&b={from_d}&c={from_y}&d={to_m}&e={to_d}&f={to_y}&g={interval}&ignore=.csv','url_params':{'stock_id':'GOOG', 'from_m':'1', 'from_d':'1', 'from_y':'2007', 'to_m':'2', 'to_d':'28', 'to_y':'2012', 'interval':'w'}}
+
+
+@app.route('/tests', methods=['GET','POST'])
+def index():
+	form = StockId(request.form)
+	rd = ysk.yh_stock_api_get(ld)
+	l_hist = js_plot(rd)
+	curr_quote = ''
+	d_last_hist = ''
+	return render_template('index.html',curr_quote=curr_quote, d_last_hist_=d_last_hist,form=form, l_hist=l_hist)
+
+def js_plot(rd):	
+	l = []
+	for item in rd['values']:
+		tl = []
+		tl.append(item[0])
+		tl.append(float(item[4]))
+		l.append(tl)
+	return l
