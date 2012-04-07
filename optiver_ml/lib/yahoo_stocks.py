624858866b45af59f8fd439dd27965ef3d925927
kfk <alessiocivitillo@gmail.com>
2012-04-07 17:06:39 +0200

    c

--- optiver_ml/lib/yahoo_stocks.py
+++ optiver_ml/lib/yahoo_stocks.py
@@ -1,12 +1,6 @@
 import urllib2 
 import calendar, time
 
-def tran_f(item):
-	print item
-	item = item.split('-')
-	item = calendar.timegm((int(item[0]),int(item[1]),int(item[2]),0,0,0,0,0))*1000
-	return item
-
 def yh_stock_api_get(d):
 	'Gets a hash table with url and url params: {url_1:{param1:xyz,parm2:zvx}}'
 	rd = {}
@@ -22,12 +16,10 @@ def yh_stock_api_get(d):
 	for row in data:
 		n+=1
 		row = row.split(',')
-		if rd['header'] != False:	
-			row[0] = tran_f(row[0])
+		if rd['header'] != False:
 			l.append(row)
 		else:
 			if n>0:
-				row[0] = tran_f(row[0])
 				l.append(row)
 			else:
 				rd['header'] = row
@@ -47,7 +39,4 @@ def d_to_html_table(d, table_id=''):
 		out_b+='</tr>\n'
 	html_table = '''<table class='table table-bordered table-striped' id='''+table_id+'''>''' + '<thead>' + out_h +'</thead>' + '<tbody>' + out_b + '</tbody>' + '</table>\n'
 	return html_table
-
-#ld = {'name':'values','url':'http://download.finance.yahoo.com/d/quotes.csv?s={stock_id}&f=nsop&e=.csv','url_params':{'stock_id':'GOOG'},'header':['name','id','open','close']}
-ld = {'url':'http://ichart.yahoo.com/table.csv?s={stock_id}&a={from_m}&b={from_d}&c={from_y}&d={to_m}&e={to_d}&f={to_y}&g={interval}&ignore=.csv','url_params':{'stock_id':'GOOG', 'from_m':'1', 'from_d':'1', 'from_y':'2007', 'to_m':'2', 'to_d':'28', 'to_y':'2012', 'interval':'w'}}
 
--- optiver_ml/views/views.py
+++ optiver_ml/views/views.py
@@ -1,46 +1,64 @@
 from optiver_ml import app
 from optiver_ml.lib import yahoo_stocks as ysk
-from flask import render_template, request
-
+from flask import render_template, request, jsonify
 from wtforms import Form, BooleanField, TextField, validators
 
-class StockId(Form):
-    stockId = TextField('StockId', [validators.Length(min=4, max=10)])
+import json 
+
+class Stock_id(Form):
+    stock_id = TextField('stock_id', [validators.Length(min=4, max=10)])
+
+dh = {'url':'http://ichart.yahoo.com/table.csv?s={stock_id}&a={from_m}&b={from_d}&c={from_y}&d={to_m}&e={to_d}&f={to_y}&g={interval}&ignore=.csv','url_params':{'stock_id':'GOOG', 'from_m':'1', 'from_d':'1', 'from_y':'2007', 'to_m':'2', 'to_d':'28', 'to_y':'2012', 'interval':'w'}, 'trans_fs':{'to_unix_timestamp'}}
+
+dc = {'name':'values','url':'http://download.finance.yahoo.com/d/quotes.csv?s={stock_id}&f=nsop&e=.csv','url_params':{'stock_id':'GOOG'},'header':['name','id','open','close']}
 
 @app.route('/', methods=['GET','POST'])
 def index():
-	form = StockId(request.form)
-	d = ysk.curr_quote('GOOG')
-	l_hist, d_last_hist = ysk.hist_quote('GOOG')
-	curr_quote = d_to_html(d)
-	d_last_hist = d_to_html(d_last_hist, table_id="'dynamic_table'")
+	form = Stock_id(request.form)
+	names = {}
+	l_hist = {}
 	if request.method == 'POST':
-		stockId = form.stockId.data
-		d = ysk.curr_quote(stockId)
-		l_hist, d_last_hist = ysk.hist_quote(stockId)
-		d_last_hist = d_to_html(d_last_hist, table_id="'dynamic_table'")
-		curr_quote = d_to_html(d)
-		return render_template('index.html', form=form, curr_quote=curr_quote, l_hist=l_hist, d_last_hist=d_last_hist)
- 
-	return render_template('index.html', form=form, curr_quote=curr_quote,l_hist=l_hist, d_last_hist=d_last_hist)
+		dh['url_params']['stock_id'] = form.stock_id.data
+		dc['url_params']['stock_id'] = form.stock_id.data
+	names['stock_id'] = '"'+dc['url_params']['stock_id']+'"'
+	rd = ysk.yh_stock_api_get(dh)
+	l_hist['data'], l_mva = js_plot(rd)
+	l_hist['label'] = names['stock_id']
+	print json.dumps(l_hist)
+	curr_quote = ysk.d_to_html_table(ysk.yh_stock_api_get(dc))
+	table_hist = ysk.d_to_html_table(rd, table_id='dynamic_table')
+	return render_template('index.html',curr_quote=curr_quote,form=form, l_hist=json.dumps(l_hist),table_hist=table_hist,names=names, l_mva=l_mva)
 
-ld = {'url':'http://ichart.yahoo.com/table.csv?s={stock_id}&a={from_m}&b={from_d}&c={from_y}&d={to_m}&e={to_d}&f={to_y}&g={interval}&ignore=.csv','url_params':{'stock_id':'GOOG', 'from_m':'1', 'from_d':'1', 'from_y':'2007', 'to_m':'2', 'to_d':'28', 'to_y':'2012', 'interval':'w'}}
 
 
-@app.route('/tests', methods=['GET','POST'])
-def index():
-	form = StockId(request.form)
-	rd = ysk.yh_stock_api_get(ld)
-	l_hist = js_plot(rd)
-	curr_quote = ''
-	d_last_hist = ''
-	return render_template('index.html',curr_quote=curr_quote, d_last_hist_=d_last_hist,form=form, l_hist=l_hist)
-
-def js_plot(rd):	
+import calendar
+@app.route('/js_plot_t')
+def js_plot_t():	
+	rd = ysk.yh_stock_api_get(dh)
 	l = []
+	lm1 = []
+	n = -1
+	m1 = 10
+	m  = [2,3,4]
+	acc1 = 0
 	for item in rd['values']:
+		n+=1
 		tl = []
-		tl.append(item[0])
+		item_s = item[0].split('-')
+		tm = calendar.timegm((int(item_s[0]),int(item_s[1]),int(item_s[2]),0,0,0,0,0))*1000
+		tl.append(tm)
 		tl.append(float(item[4]))
 		l.append(tl)
-	return l
+		tl = []
+		if n<m1:
+			acc1+=float(item[4])
+		if n == m1:
+			tl.append(tm)
+			tl.append(acc1/m1)
+			lm1.append(tl)
+			n=-1
+			acc1=0
+	j = {}
+	j['lm1']={'label':'mav_test', 'data':lm1}
+	j['stock']={'label':'test','data':l}
+	return jsonify(j)
